e1810b2a12c09561f441150b39f8130f986ba6d1
sagi <sagi.monza@capriza.com>
2014-12-07 00:49:37 +0200

    rename...

--- js/main.js
+++ js/main.js
@@ -13,46 +13,46 @@ $(document).ready(function() {
 		view && view.reload();
 	});
 
-	var fileFeedModel = new Log.Models.FileFeedClass();
+	var logFileFeedModel = new Log.Models.FileFeedClass();
 	new Log.Views.FileFeedClass({
 		el: "#logFile",
-		model: fileFeedModel,
+		model: logFileFeedModel,
 		$reloadView: $("#reloadConsole"),
 		$pickedLogFilename: $("#pickedLogFilename")
 	});
 
-	var fileFeedModel2 = new Log.Models.FileFeedClass();
+	var xmlFileFeedModel = new Log.Models.FileFeedClass();
 	new Log.Views.FileFeedClass({
 		el: "#xmlFile",
-		model: fileFeedModel2,
+		model: xmlFileFeedModel,
 		$reloadView: $("#reloadEditor"),
 		$pickedLogFilename: $("#pickedXMLFilename")
 	});
 
-	var linkFeedModel = new Log.Models.LinkFeedClass();
+	var logLinkFeedModel = new Log.Models.LinkFeedClass();
 	new Log.Views.LinkFeedClass({
 		el: "#logLink",
-		model: linkFeedModel,
+		model: logLinkFeedModel,
 		filterRegexp: /.log$/,
 		$reloadView: $("#reloadConsole"),
 		$linkOpenerView: $("#consoleLinkOpener"),
 		$pickedLogFilename: $("#pickedLogFilename")
 	});
 
-	var linkFeedModel2 = new Log.Models.LinkFeedClass();
+	var xmlLinkFeedModel = new Log.Models.LinkFeedClass();
 	new Log.Views.LinkFeedClass({
 		el: "#xmlLink",
-		model: linkFeedModel2,
+		model: xmlLinkFeedModel,
 		filterRegexp: /.xml$/,
 		$reloadView: $("#reloadEditor"),
 		$linkOpenerView: $("#xmlLinkOpener"),
 		$pickedLogFilename: $("#pickedXMLFilename")
 	});
 
-	var linkFeedModel3 = new Log.Models.LinkFeedClass();
+	var feedbackLinkFeedModel = new Log.Models.LinkFeedClass();
 	new Log.Views.LinkFeedClass({
 		$delegateViews: [$("#logLink"), $("#xmlLink")],
-		model: linkFeedModel3,
+		model: feedbackLinkFeedModel,
 		$linkOpenerView: $("#feedbackLinkOpener")
 	});
 
@@ -61,9 +61,9 @@ $(document).ready(function() {
 
 	new Log.Views.MenuSidebarClass({ el: "#menu-sidebar", $menuSidebarOpeners: [$("#console-menu-sidebar-opener"), $("#xml-menu-sidebar-opener")] });
 
-	var consoleLines = new Log.Views.ConsoleLinesClass({ dataFeedModels: [fileFeedModel, linkFeedModel], filtersModel: filtersModel });
+	var consoleLines = new Log.Views.ConsoleLinesClass({ dataFeedModels: [logFileFeedModel, logLinkFeedModel], filtersModel: filtersModel });
 
-	var editor = new Log.Views.EditorClass({ dataFeedModels: [fileFeedModel2, linkFeedModel2] });
+	var editor = new Log.Views.EditorClass({ dataFeedModels: [xmlFileFeedModel, xmlLinkFeedModel] });
 
 });
 
